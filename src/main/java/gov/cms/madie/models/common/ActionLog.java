67c45a0fa8d498fa077a9e1d5769415601486bad
Nate Moraca <nate.moraca@semanticbits.com>
2022-07-12 16:52:50 -0400

    adding shared base types for retrievable resource action logs

---
+++ src/main/java/gov/cms/madie/models/common/ActionLog.java
@@ -0,0 +1,14 @@
+package gov.cms.madie.models.common;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
+@Data
+public class ActionLog {
+  @Id
+  private String id;
+  private String targetId;
+  private List<Action> actions;
+}
