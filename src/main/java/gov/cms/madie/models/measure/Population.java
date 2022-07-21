77c4df417e7459b43551b4b3132c3c6ccd8049fa
adongare <ashok.dongare@semanticbits.com>
2022-07-21 14:08:40 -0400

    MAT-4427 add id for population, bump snapshot version

--- src/main/java/gov/cms/madie/models/measure/Population.java
+++ src/main/java/gov/cms/madie/models/measure/Population.java
@@ -4,12 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Population {
+  @Id
+  private String id;
   private PopulationType name;
   private String definition;
 }
