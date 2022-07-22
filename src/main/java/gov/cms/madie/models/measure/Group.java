e1c2c5c9367eb9ac155ec8fba22e4f92223a9675
CeciliaLiu8 <cecilialiu8@gmail.com>
2022-07-22 08:33:24 -0500

    MAT-4501 add group scoring unit

--- src/main/java/gov/cms/madie/models/measure/Group.java
+++ src/main/java/gov/cms/madie/models/measure/Group.java
@@ -38,4 +38,7 @@ public class Group implements GroupScoringPopulation {
   private String rateAggregation;
 
   private List<MeasureGroupTypes> measureGroupTypes;
+  
+  private String scoringUnit;
+  
 }
