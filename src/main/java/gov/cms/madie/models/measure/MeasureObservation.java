3ae2ac79b7344c6683e065d126c68c3b00f7b9fb
Nate Moraca <nate.moraca@semanticbits.com>
2022-08-04 18:29:13 -0400

    add support for measure observation on measure group, with stubbed validator to be implemented later

---
+++ src/main/java/gov/cms/madie/models/measure/MeasureObservation.java
@@ -0,0 +1,33 @@
+package gov.cms.madie.models.measure;
+
+import gov.cms.madie.models.AggregateMethodType;
+import gov.cms.madie.models.validators.EnumValidator;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class MeasureObservation {
+  private String id;
+  @NotBlank(
+      message = "Observation definition is required.",
+      groups = {Measure.ValidationOrder5.class}
+  )
+  private String definition;
+  private String criteriaReference;
+  @NotNull(
+      message = "Aggregate Method is required.",
+      groups = {Measure.ValidationOrder5.class})
+  @EnumValidator(
+      enumClass = AggregateMethodType.class,
+      message = "Aggregate Method must be a valid MADiE Aggregate Method type",
+      groups = {Measure.ValidationOrder5.class})
+  private String aggregateMethod;
+}
---
+++ src/main/java/gov/cms/madie/models/validators/ValidMeasureObservation.java
@@ -0,0 +1,21 @@
+package gov.cms.madie.models.validators;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = MeasureObservationValidator.class)
+@Documented
+public @interface ValidMeasureObservation {
+  String message() default "Measure observations are invalid for the chosen scoring type";
+
+  Class<?>[] groups() default {};
+
+  Class<? extends Payload>[] payload() default {};
+}
