bf755a5febd94ea6e791f4d6db3cac4cd3f1b151
sb-prateekkeerthi <prateek.keerthi@semanticbits.com>
2022-09-27 11:47:06 -0400

    save stratification values

---
+++ src/main/java/gov/cms/madie/models/measure/TestCaseStratificationValue.java
@@ -0,0 +1,18 @@
+package gov.cms.madie.models.measure;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TestCaseStratificationValue {
+    private String id;
+    private String name;
+    private Object expected;
+    private Object actual;
+}
+
