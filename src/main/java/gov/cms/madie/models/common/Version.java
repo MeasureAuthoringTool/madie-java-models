411e9d21b551d957b8c1f284a1215842eca41867
CeciliaLiu8 <cecilialiu8@gmail.com>
2023-01-09 10:03:06 -0600

    MAT-5191 refactor measure version - version as common object

---
+++ src/main/java/gov/cms/madie/models/common/Version.java
@@ -0,0 +1,41 @@
+package gov.cms.madie.models.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Version {
+  private int major;
+  private int minor;
+  private int revisionNumber;
+
+  public static Version parse(String versionStr) {
+    if (versionStr == null || versionStr.isEmpty()) {
+      return new Version();
+    }
+
+    String[] parts = versionStr.split("\\.");
+
+    return Version.builder()
+        .major(Integer.parseInt(parts[0]))
+        .minor(parts.length > 1 ? Integer.parseInt(parts[1]) : 0)
+        .revisionNumber(parts.length > 2 ? Integer.parseInt(parts[2]) : 0)
+        .build();
+  }
+
+  @Override
+  public String toString() {
+    return this.getMajor()
+        + "."
+        + this.getMinor()
+        + "."
+        + String.format("%03d", this.getRevisionNumber());
+  }
+}
