4eb11bc2f067aa46973103468bd3d3761de4bce2
CeciliaLiu8 <cecilialiu8@gmail.com>
2023-04-26 07:13:27 -0500

    remove qdm scoring required validation

--- src/main/java/gov/cms/madie/models/measure/QdmMeasure.java
+++ src/main/java/gov/cms/madie/models/measure/QdmMeasure.java
@@ -1,10 +1,10 @@
 package gov.cms.madie.models.measure;
 
-
-import javax.validation.constraints.NotBlank;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
+import gov.cms.madie.models.validators.EnumValidator;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -17,7 +17,17 @@ import lombok.experimental.SuperBuilder;
 @ToString(callSuper=true)
 public class QdmMeasure extends Measure {
 
-	@NotBlank(message = "Scoring is required for QDM Measure.")
+	@EnumValidator(
+      enumClass = MeasureScoring.class,
+      message = "Scoring must be a valid MADiE scoring type"
+  )
 	private String scoring;
 	
+	@Override
+  public void setGroups(List<Group> groups) {
+    if (groups.stream().map(Group::getScoring).allMatch(scoring -> groups.get(0).getScoring().equalsIgnoreCase(scoring))
+    		&& groups.get(0).getScoring().equalsIgnoreCase(this.scoring)) {
+      super.setGroups(groups);
+    } else throw new RuntimeException("Groups must have same scoring");
+  }
 }
