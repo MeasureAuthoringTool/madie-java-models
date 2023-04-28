a606d16cb6235a67bfe0aea8079dd92698b3507a
CeciliaLiu8 <cecilialiu8@gmail.com>
2023-04-28 12:36:38 -0500

    upgrade to jakarta validation

--- src/main/java/gov/cms/madie/models/measure/Measure.java
+++ src/main/java/gov/cms/madie/models/measure/Measure.java
@@ -125,6 +125,7 @@ public class Measure extends ResourceAcl {
   private List<DefDescPair> riskAdjustments;
   private ProgramUseContext programUseContext;
 
+  @NotBlank(message = "Model is required")
   @EnumValidator(
       enumClass = ModelType.class,
       message = "MADiE was unable to complete your request, please try again.",
