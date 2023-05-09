c3faa619c226eaf4c5a5bb07aa311148b9437e50
CeciliaLiu8 <cecilialiu8@gmail.com>
2023-05-09 08:11:22 -0500

    MAT-5537 validate qdm population criteria

--- src/main/java/gov/cms/madie/models/measure/Group.java
+++ src/main/java/gov/cms/madie/models/measure/Group.java
@@ -10,8 +10,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
@@ -53,8 +51,6 @@ public class Group implements GroupScoringPopulation {
   @Valid
   private List<Stratification> stratifications;
 
-  @NotBlank(
-     message = "Population Basis is required.")
   private String populationBasis;
 
 }
