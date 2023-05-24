0a459990439752b654869c908c34690eb598923a
Rohit Kandimalla <rohit.kandimalla@semanticbits.com>
2023-05-24 15:13:22 -0400

    Excluding just ValidScoringPopulation

--- src/main/java/gov/cms/madie/models/measure/TestCaseGroupPopulation.java
+++ src/main/java/gov/cms/madie/models/measure/TestCaseGroupPopulation.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Builder(toBuilder = true)
 @NoArgsConstructor
 @AllArgsConstructor
-@ValidScoringPopulation
+//@ValidScoringPopulation
 public class TestCaseGroupPopulation {
   private String groupId;
 
