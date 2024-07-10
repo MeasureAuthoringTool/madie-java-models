fc444a06a6d5e752cb2e613ba8ae0623f3388e8c
Cecilia Liu <cecilia.liu@icf.com>
2024-07-10 11:32:06 -0500

    MAT-7155 add birthDatetime

--- src/main/java/gov/cms/madie/models/cqm/datacriteria/basetypes/TestCaseJson.java
+++ src/main/java/gov/cms/madie/models/cqm/datacriteria/basetypes/TestCaseJson.java
@@ -1,6 +1,11 @@
 package gov.cms.madie.models.cqm.datacriteria.basetypes;
 
+import java.time.ZonedDateTime;
 import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,4 +24,17 @@ public class TestCaseJson {
   private String _id;
   private String qdmVersion = "5.6";
   private List<DataElement> dataElements;
+
+  @JsonFormat(
+      shape = JsonFormat.Shape.STRING,
+      pattern = LocalDateTimeFormatConstant.LOCAL_DATE_TIME_PATTERN)
+  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+  private ZonedDateTime birthDatetime;
+
+  public ZonedDateTime shiftDateByYear(int year) {
+    if (this.birthDatetime == null) {
+      return null;
+    }
+    return this.birthDatetime.plusYears(year);
+  }
 }
