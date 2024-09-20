9eea2ecf2c3d58605cec9ef6b755c237f37b81b5
Christopher Hubert <christopher.hubert@semanticbits.com>
2024-09-20 14:55:26 -0400

    MAT-7603: add case number and sequence

---
+++ src/main/java/gov/cms/madie/models/measure/TestCaseSequence.java
@@ -0,0 +1,18 @@
+package gov.cms.madie.models.measure;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
+public class TestCaseSequence {
+  @Id private String id;
+  private int sequence;
+}
