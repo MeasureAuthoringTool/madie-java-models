ebb778516d55649a995da194eca0056d9afd3529
Dongare <42984@icfgqgc4n6jqq.mynetworksettings.com>
2024-09-23 13:09:27 -0400

    MAT-7058 add required validations for organization

--- src/main/java/gov/cms/madie/models/common/Organization.java
+++ src/main/java/gov/cms/madie/models/common/Organization.java
@@ -1,5 +1,6 @@
 package gov.cms.madie.models.common;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,8 +14,13 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @AllArgsConstructor
 @Document(collection = "organization")
 public class Organization {
-  @Id private String id;
+  @Id
+  @NotBlank(message = "Id is required.")
+  private String id;
+
+  @NotBlank(message = "Name is required.")
   private String name;
+
   private String oid;
   private String url;
 }
