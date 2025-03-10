98e9d7b4a2afcef0179e3a5655e8eb55ba27fd36
sb-cecilialiu <cecilia.liu@icf.com>
2025-03-10 15:23:44 -0500

    MAT-8277 update version

--- src/main/java/gov/cms/madie/models/measure/Export.java
+++ src/main/java/gov/cms/madie/models/measure/Export.java
@@ -12,12 +12,12 @@ import org.springframework.data.annotation.Id;
 @AllArgsConstructor
 // TODO: look into GridFS API as our exports may grow larger than the MongoDB document max of 16MB
 public class Export {
-    @Id private String id;
-    private String measureId;
-    private String measureBundleJson;
-    private String measureBundleGridFsId;
-    private String measureBundleJsonWithoutWarnings;
-    private String measureBundleWithoutWarningsGridFsId;
-    private String humanReadable;
-    private byte[] packageData;
+  @Id private String id;
+  private String measureId;
+  private String measureBundleJson;
+  private String measureBundleGridFsId;
+  private String measureBundleJsonWithoutWarnings;
+  private String measureBundleWithoutWarningsGridFsId;
+  private String humanReadable;
+  private byte[] packageData;
 }
