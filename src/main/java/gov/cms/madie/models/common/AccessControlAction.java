eb5cbdf25d53a899d29548357df1ced41a6639a3
Edwin Tan <N119494@icf.com>
2025-03-18 11:01:49 -0700

    MAT-8205: model updates for saving/updating shared measures

--- src/main/java/gov/cms/madie/models/common/AccessControlAction.java
+++ src/main/java/gov/cms/madie/models/common/AccessControlAction.java
@@ -4,8 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Document
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
