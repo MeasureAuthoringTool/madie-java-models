f4175230ea71db05e52c288702520e544a63fa66
sb-prateekkeerthi <prateekkeerthi7@gmail.com>
2026-08-05 11:48:02 -0400

    MAT-10244v1 fixed the name for US Core v6.1.0-derived"

--- src/main/java/gov/cms/madie/models/utils/VersionConstants.java
+++ src/main/java/gov/cms/madie/models/utils/VersionConstants.java
@@ -7,6 +7,6 @@ public final class VersionConstants {
   public static final String QICORE_6_0_0_VERSION = "QI-Core v6.0.0";
   public static final String QICORE_7_0_0_VERSION = "QI-Core v7.0.0";
   public static final String QICORE_7_0_2_VERSION = "QI-Core v7.0.2";
-  public static final String US_CORE_6_1_0_VERSION = "US-Core v6.1.0-derived";
+  public static final String US_CORE_6_1_0_VERSION = "US Core v6.1.0-derived";
   public static final String USQUALITYCORE_0_5_0_Version = "US Quality Core v0.5.0";
 }
