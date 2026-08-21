2f113c2c892f0dbb4211f6707df3116e966031f1
sb-prateekkeerthi <prateekkeerthi7@gmail.com>
2026-08-21 09:50:55 -0400

    MAT-10192 removed unwanted code

--- src/main/java/gov/cms/madie/models/common/ReviewScope.java
+++ src/main/java/gov/cms/madie/models/common/ReviewScope.java
@@ -1,12 +1,6 @@
 package gov.cms.madie.models.common;
 
-/**
- * Which slice of the review work a review search asks for, the review counterpart of {@link
- * OwnershipType} on the measure and library searches.
- */
 public enum ReviewScope {
-  /** Everything under review, whoever it is assigned to: the "All Reviews" tab. */
   ALL,
-  /** Only what is assigned to the requesting reviewer and not finished: the "My Reviews" tab. */
   ASSIGNED
 }
