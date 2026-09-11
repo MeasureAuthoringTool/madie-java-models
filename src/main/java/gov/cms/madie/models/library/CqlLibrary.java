14e1e6927d6489daaf2fa49da8541e9ba5406fd2
nmorasb <nate.moraca@semanticbits.com>
2026-09-11 11:52:06 -0400

    MAT-10331: remove old custom validator for library name validation and use regular pattern; match validation message to draft message

--- src/main/java/gov/cms/madie/models/library/CqlLibrary.java
+++ src/main/java/gov/cms/madie/models/library/CqlLibrary.java
@@ -6,10 +6,8 @@ import tools.jackson.databind.annotation.JsonSerialize;
 import gov.cms.madie.models.common.IncludedLibrary;
 import gov.cms.madie.models.common.ModelType;
 import gov.cms.madie.models.validators.EnumValidator;
-import gov.cms.madie.models.validators.ValidLibraryName;
 import gov.cms.madie.models.validators.XssFilter;
 import gov.cms.madie.models.utils.VersionJsonSerializer;
-import gov.cms.madie.models.common.ModelType;
 import gov.cms.madie.models.common.Version;
 
 import java.time.Instant;
@@ -30,7 +28,6 @@ import org.springframework.data.mongodb.core.index.Indexed;
 @Builder(toBuilder = true)
 @NoArgsConstructor
 @AllArgsConstructor
-@ValidLibraryName
 public class CqlLibrary {
   @Id private String id;
 
@@ -48,14 +45,12 @@ public class CqlLibrary {
       groups = {CqlLibrary.ValidationOrder2.class},
       message = "Library name cannot be more than 64 characters.")
   @Pattern(
-      regexp = "^[^_]+$",
+      regexp = "^[A-Z][a-zA-Z0-9]*$",
       groups = {CqlLibrary.ValidationOrder3.class},
-      message = "Library Name can not contain underscores.")
-  @Pattern(
-      regexp = "^[A-Z][a-zA-Z0-9]+$",
-      groups = {CqlLibrary.ValidationOrder4.class},
       message =
-          "Library Name must start with an uppercase letter and can only contain alphanumeric characters.")
+          "Library name must start with an upper case letter, "
+              + "followed by alpha-numeric character(s) and must not contain "
+              + "spaces or other special characters.")
   @XssFilter
   @Indexed
   private String cqlLibraryName;
